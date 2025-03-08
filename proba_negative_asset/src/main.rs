c6c13228be78d498f28d1b49075905510840e5d7
chataignault <leonchat@orange.fr>
2025-03-08 12:28:45 +0000

    proba negative analytic

--- proba_negative_asset/src/main.rs
+++ proba_negative_asset/src/main.rs
@@ -4,7 +4,6 @@ extern crate approx;
 use clap::Parser;
 use rand::distr::{Bernoulli, Distribution};
 use std::error::Error;
-// use approx::abs_diff_eq;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -14,14 +13,14 @@ struct Args {
     x: f32,
 }
 
-fn compute_proba_negative_mc(n: u32, p: f64, s: f32, x: f32) -> f32 {
+fn compute_proba_negative_mc(n: u32, p: f64, s: f32, ds: f32) -> f64 {
     // Monte Carlo estimate of the path-dependant probability
-    let r: i32 = (s / x).ceil() as i32;
+    let r: i32 = (s / ds).ceil() as i32;
     let mut rng = rand::rng();
     let d = Bernoulli::new(p).unwrap();
     const N: u32 = 10_000;
     let mut p_hat: u32 = 0;
-    for _ in 1..N {
+    for _ in 0..N {
         let mut sim: Vec<f64> = d
             .sample_iter(&mut rng)
             .take(usize::try_from(n).unwrap())
@@ -31,24 +30,29 @@ fn compute_proba_negative_mc(n: u32, p: f64, s: f32, x: f32) -> f32 {
             .collect();
         p_hat += sim
             .iter_mut()
-            .fold((r < 0, r as f64), |acc, x| {
+            .fold((r <= 0, r as f64), |acc, x| {
                 *x += acc.1;
-                (acc.0 || (*x < 0.), *x)
+                (acc.0 || (*x <= 0.), *x)
             })
             .0 as u32;
     }
-    p_hat as f32 / N as f32
+    p_hat as f64 / N as f64
 }
 
-fn compute_proba_negative(n: u32, p: f64, s: f32, x: f32) -> f32 {
-    let d: i32 = ((s as f32) / (x as f32)).ceil() as i32;
-    if d <= 0 {
+fn compute_proba_negative(n: u32, p: f64, s: f32, ds: f32) -> f64 {
+    let r: i32 = (s / ds).ceil() as i32;
+    let mut p_hat: f64 = 0.;
+    if r <= 0 {
         return 1.;
+    } else if n as i32 - r < 0 {
+        return 0.;
     } else {
-        // compute hitting time expectation and apply d times
-        return 0.; // todo
+        let max_steps_up: u32 = (n as i32 - r).try_into().unwrap();
+        for k in 1..(max_steps_up / 2 + 2) {
+            p_hat += p.powf(k.into()) * (1. - p).powf((r + k as i32 - 2).into());
+        }
     }
-    1.
+    p_hat
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -56,7 +60,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // using the stopping time of going lower than initially by one step
     let args = Args::parse();
     println!(
-        "Steps: {}\nProba up: {}, Start value: {}, Price step: {}",
+        "Steps: {}\nProba up: {}, \nStart value: {}, \nPrice step: {}",
         args.n, args.p, args.s, args.x
     );
     println!(
@@ -74,7 +78,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 mod tests {
     use super::*;
 
-    const TOL: f32 = 1e-3;
+    const TOL: f64 = 1e-3;
 
     #[test]
     fn test_mc_trivial_cases() {
@@ -93,6 +97,16 @@ mod tests {
             0.5,
             epsilon = TOL
         );
+        _ = abs_diff_eq!(
+            compute_proba_negative_mc(0, 0.5, 50., 60.),
+            0.,
+            epsilon = TOL
+        );
+        _ = abs_diff_eq!(
+            compute_proba_negative_mc(0, 0.5, -50., 60.),
+            1.,
+            epsilon = TOL
+        );
     }
 
     #[test]
@@ -100,6 +114,8 @@ mod tests {
         _ = abs_diff_eq!(compute_proba_negative(1, 0.5, -39., 20.), 1., epsilon = TOL);
         _ = abs_diff_eq!(compute_proba_negative(1, 0.5, 100., 20.), 0., epsilon = TOL);
         _ = abs_diff_eq!(compute_proba_negative(1, 0.5, 50., 60.), 0.5, epsilon = TOL);
+        _ = abs_diff_eq!(compute_proba_negative(0, 0.5, 50., 60.), 0., epsilon = TOL);
+        _ = abs_diff_eq!(compute_proba_negative(0, 0.5, -50., 60.), 1., epsilon = TOL);
     }
 
     #[test]
@@ -112,6 +128,11 @@ mod tests {
         _ = abs_diff_eq!(
             compute_proba_negative(20, 0.4, 350., 39.),
             compute_proba_negative_mc(20, 0.4, 350., 39.),
+            epsilon = TOL
+        );
+        _ = abs_diff_eq!(
+            compute_proba_negative(30, 0.3, 350., 39.),
+            compute_proba_negative_mc(30, 0.3, 350., 39.),
             epsilon = TOL
         );
     }
