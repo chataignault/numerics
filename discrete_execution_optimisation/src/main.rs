813cb25fa4180b35b713083d27d86a2966dc0ac0
chataignault <leonchat@orange.fr>
2025-03-11 11:33:30 +0000

    start impl execution

--- discrete_execution_optimisation/src/main.rs
+++ discrete_execution_optimisation/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::error::Error;
+
+
+#[derive(Parser, Debug)]
+struct Args {
+    n: u32,
+    k: u32,
+    a: f32,
+    b: f32,
+    p: f32
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    println!("n: {}\nk: {}\na,b: {}, {}\np: {}", args.n, args.k, args.a, args.b, args.p);
+    Ok(())
 }
