4856b43c9b6f8440063ce80598136649b0c0e370
chataignault <leonchat@orange.fr>
2025-03-27 13:16:23 +0000

    add efficient integer partition algorithm

--- proba_negative_asset/src/int_part.rs
+++ proba_negative_asset/src/int_part.rs
@@ -1,35 +1,5 @@
 pub fn all_partitions(n: u32) -> impl Iterator<Item = Vec<u32>> {
     // Generate all possible partitions first
-    fn generate_partitions(n: u32) -> Vec<Vec<u32>> {
-        if n == 0 {
-            return vec![vec![]];
-        }
-        if n == 1 {
-            return vec![vec![1]];
-        }
-
-        let mut result = Vec::new();
-
-        // Try each possible first part
-        for first in 1..=n {
-            // Recursively generate all partitions of the remainder
-            let sub_partitions = generate_partitions(n - first);
-
-            for partition in sub_partitions {
-                // Try inserting 'first' at each possible position
-                for i in 0..=partition.len() {
-                    let mut new_partition = partition.clone();
-                    new_partition.insert(i, first);
-                    result.push(new_partition);
-                }
-            }
-        }
-
-        // Deduplicate
-        result.sort();
-        result.dedup();
-        result
-    }
 
     let partitions = generate_partitions(n);
     let mut index = 0;
@@ -43,6 +13,37 @@ pub fn all_partitions(n: u32) -> impl Iterator<Item = Vec<u32>> {
             None
         }
     })
+}
+
+fn generate_partitions(n: u32) -> Vec<Vec<u32>> {
+    if n == 0 {
+        return vec![vec![]];
+    }
+    if n == 1 {
+        return vec![vec![1]];
+    }
+
+    let mut result = Vec::new();
+
+    // Try each possible first part
+    for first in 1..=n {
+        // Recursively generate all partitions of the remainder
+        let sub_partitions = generate_partitions(n - first);
+
+        for partition in sub_partitions {
+            // Try inserting 'first' at each possible position
+            for i in 0..=partition.len() {
+                let mut new_partition = partition.clone();
+                new_partition.insert(i, first);
+                result.push(new_partition);
+            }
+        }
+    }
+
+    // Deduplicate
+    result.sort();
+    result.dedup();
+    result
 }
 
 pub fn simple_ascending(n: u32) -> impl Iterator<Item = Vec<u32>> {
@@ -61,6 +62,35 @@ pub fn simple_ascending(n: u32) -> impl Iterator<Item = Vec<u32>> {
             k += 1;
         }
         a[k] = x + y;
+        part.push(a[..(k + 1)].to_vec());
+    }
+    part.into_iter()
+}
+
+pub fn efficient_ascending(n: u32) -> impl Iterator<Item = Vec<u32>> {
+    // from https://jeromekelleher.net/generating-integer-partitions
+    let mut part: Vec<Vec<u32>> = vec![];
+    let mut a: Vec<u32> = vec![0; n.try_into().unwrap()];
+    let mut k: usize = 1;
+    let mut y: u32 = n - 1;
+    while k > 0 {
+        let mut x = a[k - 1] + 1;
+        k -= 1;
+        while 2 * x <= y {
+            a[k] = x;
+            y -= x;
+            k += 1;
+        }
+        let l: usize = k + 1;
+        while x <= y {
+            a[k] = x;
+            a[l] = y;
+            part.push(a[..(k + 2)].to_vec());
+            x += 1;
+            y -= 1;
+        }
+        a[k] = x + y;
+        y = x + y - 1;
         part.push(a[..(k + 1)].to_vec());
     }
     part.into_iter()
