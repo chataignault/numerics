1f45b89573a89cadb9e8fcaf1dcaa135bf4de13f
chataignault <leonchat@orange.fr>
2025-11-20 14:54:46 +0000

    codeforces: rust leg count

---
+++ codeforces/21_farm_legs/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let mut nums = input.split_whitespace();
+    let N: i32 = nums.next().unwrap().parse().unwrap();
+    for _ in 0..N {
+        let mut l = String::new();
+        std::io::stdin().read_line(&mut l).unwrap();
+        let k:i32 = l.split_whitespace().next().unwrap().parse().unwrap();
+        if k % 2 == 1 {
+            println!("0");
+        } else {
+            println!("{}", k / 4 + 1);
+        }
+    }
+}
+
