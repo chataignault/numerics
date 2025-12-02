434159bc9cfb6742aff545277e9317b45269864d
chataignault <leonchat@orange.fr>
2025-12-02 10:15:08 +0000

    max die: reformat mc

--- max_die/src/main.rs
+++ max_die/src/main.rs
@@ -31,29 +31,7 @@ fn validate_args(p: i32, q: i32) {
     }
 }
 
-fn compute_optimal_turns(game_variance: f64, std_limit: f64) -> i64 {
-    // compute portfolio weight
-    let w: i64 = (std_limit * std_limit / game_variance).floor() as i64;
-    w
-}
-
-fn main() {
-    let args = Args::parse();
-    dbg!(&args);
-
-    let n = args.n;
-    let p = args.p;
-    let q = args.q;
-    let stdmax = args.stdmax;
-
-    validate_args(p, q);
-
-    println!("Inputs :");
-    println!("- n MC samples : {}", n);
-    println!("- p, q for uniform draws : {}, {}", p, q);
-    println!();
-
-    println!("Compute Monte Carlo estimates :");
+fn simulate_exp_var(p: i32, q: i32, n: i32) -> (f64, f64) {
     let x_dist = Uniform::new_inclusive(1, p).unwrap();
     let y_dist = Uniform::new_inclusive(1, q).unwrap();
     let mut s: i64 = 0;
@@ -73,17 +51,16 @@ fn main() {
     let exp_p_simulated: f64 = (s as f64) / (n as f64);
     let exp_p_squared: f64 = (v as f64) / (n as f64);
     let exp_var: f64 = exp_p_squared - exp_p_simulated * exp_p_simulated;
+    (exp_p_simulated, exp_var)
+}
 
-    let exp_p_analytical: f64 = ((p * (p + 1) - q * (q + 1)) as f64) / (2. * p as f64);
+fn compute_optimal_turns(game_variance: f64, std_limit: f64) -> i64 {
+    // compute portfolio weight
+    let w: i64 = (std_limit * std_limit / game_variance).floor() as i64;
+    w
+}
 
-    println!("===NUMERICAL VERIFICATION===");
-    println!("Expected return : {}", exp_p_analytical);
-    println!("Empirical mean return : {}", exp_p_simulated);
-    println!();
-    println!("Simulated variance : {}", exp_var);
-    println!("Simulated standard deviation : {}", exp_var.sqrt());
-    println!();
-
+fn compute_game_variance(p: i32, q: i32, mu: f64) -> f64 {
     // Compute E[P²] analytically
     let exp_p_squared_analytical = (3 * q * (q + 1) * (q + 1) - 2 * (q + 1) * (2 * q + 1)
         + p * (p + 1) * (2 * p + 1)
@@ -91,7 +68,43 @@ fn main() {
         / (6 * p) as f64;
 
     // Deduce analytical variance
-    let variance_analytical: f64 = exp_p_squared_analytical - exp_p_analytical * exp_p_analytical;
+    exp_p_squared_analytical - mu * mu
+}
+
+fn main() {
+    let args = Args::parse();
+    dbg!(&args);
+
+    let n = args.n;
+    let p = args.p;
+    let q = args.q;
+    let stdmax = args.stdmax;
+
+    validate_args(p, q);
+
+    println!("Inputs :");
+    println!("- n MC samples : {}", n);
+    println!("- p, q for uniform draws : {}, {}", p, q);
+    println!();
+
+    println!("Compute Monte Carlo estimates :");
+
+    let (exp_p_simulated, exp_var_simulated) = simulate_exp_var(p, q, n);
+
+    let exp_p_analytical: f64 = ((p * (p + 1) - q * (q + 1)) as f64) / (2. * p as f64);
+
+    println!("===NUMERICAL VERIFICATION===");
+    println!("Expected return : {}", exp_p_analytical);
+    println!("Empirical mean return : {}", exp_p_simulated);
+    println!();
+    println!("Simulated variance : {}", exp_var_simulated);
+    println!(
+        "Simulated standard deviation : {}",
+        exp_var_simulated.sqrt()
+    );
+    println!();
+
+    let variance_analytical = compute_game_variance(p, q, exp_p_analytical);
 
     println!("Analytical E[P] : {}", exp_p_analytical);
     println!("Analytical Var(P) : {}", variance_analytical);
