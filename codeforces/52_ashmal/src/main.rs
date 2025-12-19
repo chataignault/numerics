23f818b28c99b6b6d74469c8cb699e0f5f53ca4b
chataignault <leonchat@orange.fr>
2025-12-19 15:43:04 +0000

    codeforces: ashmal

---
+++ codeforces/52_ashmal/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let n: u32 = input.split_whitespace().next().unwrap().parse().unwrap();
+
+    for i in 0..n {
+        let mut m_ = String::new();
+        std::io::stdin().read_line(&mut m_).unwrap();
+        let m: u32 = m_.split_whitespace().next().unwrap().parse().unwrap();
+
+        let mut l = String::new();
+        std::io::stdin().read_line(&mut l).unwrap();
+        let words: Vec<String> = l.split_whitespace().map(|s| s.to_string()).collect();
+
+        let s = words.iter().fold(String::new(), |acc, word| {
+            let prepend = format!("{}{}", word, acc);
+            let append = format!("{}{}", acc, word);
+            if prepend < append {
+                prepend
+            } else {
+                append
+            }
+        });
+        println!("{}", s);
+    }
+}
