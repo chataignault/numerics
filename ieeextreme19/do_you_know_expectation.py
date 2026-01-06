d74b55ab4270be1bc344824811b28feb93afcd2f
chataignault <leonchat@orange.fr>
2026-01-06 14:30:38 +0000

    ieeextreme19: fix do you know expectation

--- ieeextreme19/do_you_know_expectation.py
+++ ieeextreme19/do_you_know_expectation.py
@@ -1,28 +1,27 @@
 from itertools import combinations
 from functools import reduce
-import operator 
+import operator
 
 n, k = [int(c) for c in input().strip().split()]
 l = [int(i) for i in input().strip().split()]
 
-
-
-mk = 0.
-N = 2**n - 1
-for j in range(n):
-    for c in combinations(range(1,n), j):
+mk = 0.0
+N = 2**n
+for j in range(n + 1):
+    for c in combinations(range(n), j):
         sub_l = [l[i] for i in c]
         if len(sub_l):
             mk += reduce(operator.xor, sub_l) ** k
 
 s = str(mk / N)
 
-ndec = len(s.split('.')[-1])
+# format output with two decimals
+ndec = len(s.split(".")[-1])
 if ndec == 0:
     s += "00"
 elif ndec == 1:
     s += "0"
 else:
-    s = s[:2-ndec]
-    
+    s = s[: 2 - ndec]
+
 print(s)
